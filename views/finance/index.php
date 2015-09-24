f83524a987e07157115fa7e19e60515b1975cc69
Francis Albert <francis@virtual-wonders.com>
2015-09-25 02:10:16 +0800

    NEW Finance Updated the overview of the finance Added the plus and minus button Added the query when finance data is empty

--- views/finance/index.php
+++ views/finance/index.php
@@ -1,3 +1,48 @@
-<?php foreach($finances as $finance): ?>
-	<?php echo $finance->description; ?>
-<?php endforeach; ?>
+<?php 
+use yii\helpers\Html;
+use yii\widgets\LinkPager;
+?>
+<div class="container">
+	<div class="row">
+		<div class="col-md-6 col-md-offset-3 p-all">
+			<div class="p-all main-green text20">
+				Finance
+				<div class="pull-right">					
+					<button type="button" class="btn btn-primary" aria-label="Income an Equity">
+					  <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+					</button>
+					<button type="button" class="btn btn-danger" aria-label="Expense">
+					  <span class="glyphicon glyphicon-minus" aria-hidden="true"></span>
+					</button>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+
+			<div>
+				<table id="finance-table">
+					<tr class="secondary-green">
+						<th width="60%">Item</th>
+						<th width="30%">Cost</th>
+						<th width="10%">Action</th>
+					</tr>
+					<?php if(count($finances)>0): ?>				
+						<?php foreach($finances as $finance): ?>
+							<tr>
+								<td><?= Html::encode($finance->description); ?></td>
+								<td><?= $finance->cost ?></td>
+								<td> 
+									<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> 
+									<span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
+								</td>
+							</tr>
+						<?php endforeach; ?>
+					<?php else: ?>
+						<tr>
+							<td colspan="3" class="text-center">No Equities found. Please add a data.</td>
+						</tr>
+					<?php endif; ?>
+				</table>				
+			</div>
+		</div>
+	</div>
+</div>
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -30,14 +30,14 @@ AppAsset::register($this);
         'brandLabel' => 'Monday',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
-            'class' => 'navbar-inverse navbar-fixed-left',
+            'class' => 'navbar-inverse navbar-fixed-top',
         ],
     ]);
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
+            ['label' => 'Finance', 'url' => ['/finance']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ?
                 ['label' => 'Login', 'url' => ['/site/login']] :
@@ -58,14 +58,6 @@ AppAsset::register($this);
         <?= $content ?>
     </div>
 </div>
-
-<footer class="footer">
-    <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-
-        <p class="pull-right"><?= Yii::powered() ?></p>
-    </div>
-</footer>
 
 <?php $this->endBody() ?>
 </body>
